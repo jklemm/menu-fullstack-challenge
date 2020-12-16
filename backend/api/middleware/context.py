ef4c7c6b6c9a3c6c0dc105166fd8905f3e6573cb
Jorge Klemm <jorgeklemm@gmail.com>
2020-12-15 22:05:46 -0300

    Adiciona middleware controlador de Request

---
+++ backend/api/middleware/context.py
@@ -0,0 +1,9 @@
+import uuid
+
+
+class ContextMiddleware(object):
+    def process_request(self, req, resp):
+        if not req.context.get('request_id'):
+            req.context['request_id'] = str(uuid.uuid4())
+
+        resp.set_header('request-id', req.context['request_id'])
