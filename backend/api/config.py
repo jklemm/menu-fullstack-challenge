df15d584e65287e2dbc226005c030bf5e3a23eba
Jorge Klemm <jorgeklemm@gmail.com>
2020-12-15 22:06:43 -0300

    Adiciona configurador e configurações

---
+++ backend/api/config.py
@@ -0,0 +1,22 @@
+from aumbry import Attr, JsonConfig
+
+
+class DatabaseConfig(JsonConfig):
+    __mapping__ = {
+        'connection': Attr('connection', str),
+    }
+
+    connection = ''
+
+
+class AppConfig(JsonConfig):
+    __mapping__ = {
+        'db': Attr('db', DatabaseConfig),
+        'db_test': Attr('db_test', DatabaseConfig),
+        'gunicorn': Attr('gunicorn', dict),
+    }
+
+    def __init__(self):
+        self.db = DatabaseConfig()
+        self.db_test = DatabaseConfig()
+        self.gunicorn = {}
