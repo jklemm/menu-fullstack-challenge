b82cc4b3f4d13f3b9c58f0af98055e04f41b8211
Jorge Klemm <jorgeklemm@gmail.com>
2020-12-15 22:09:36 -0300

    Remove lógica de DB do arquivo de models

--- backend/api/database/models.py
+++ backend/api/database/models.py
@@ -1,26 +1,5 @@
-import os
-
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-import settings
-
-if os.environ["ENV"] == "TEST":
-    engine = create_engine(
-        '{engine}://{username}:{password}@{host}:{port}/{db_name}'.format(
-            **settings.MYSQL_TEST
-        )
-    )
-else:
-    engine = create_engine(
-        '{engine}://{username}:{password}@{host}:{port}/{db_name}'.format(
-            **settings.MYSQL_PROD
-        )
-    )
-
-session_factory = sessionmaker(bind=engine)
-Session = scoped_session(session_factory)
 
 SqlAlchemyModel = declarative_base()
 
@@ -42,7 +21,8 @@ class Pedido(SqlAlchemyModel):
             self.id, self.data, self.cliente_id, self.valor
         )
 
-    def to_json(self):
+    @property
+    def as_dict(self):
         return {
             'id': self.id,
             'data': str(self.data.isoformat()),
@@ -56,20 +36,18 @@ class Cliente(SqlAlchemyModel):
     id = Column(Integer, primary_key=True)
     primeiro_nome = Column(String(100))
     ultimo_nome = Column(String(100))
-    email = Column(String(255))
+    email = Column(String(255), unique=True)
 
     def __repr__(self):
         return "<Cliente(id={}, first_name='{}', last_name='{}', email='{}')>".format(
             self.id, self.primeiro_nome, self.ultimo_nome, self.email
         )
 
-    def to_json(self):
+    @property
+    def as_dict(self):
         return {
             'id': self.id,
             'primeiro_nome': self.primeiro_nome,
             'ultimo_nome': self.ultimo_nome,
             'email': self.email,
         }
-
-
-SqlAlchemyModel.metadata.create_all(engine)
