0af5a942bc0f265fe744c613af523340ecbc79f2
Jorge Klemm <jorgeklemm@gmail.com>
2020-12-15 22:09:42 -0300

    Adiciona manager para banco de dados

---
+++ backend/api/database/manager.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy.orm import scoping
+
+from api.database import models
+
+
+class DBManager(object):
+    def __init__(self, connection=None):
+        self.connection = connection
+
+        self.engine = sqlalchemy.create_engine(self.connection)
+        self.DBSession = scoping.scoped_session(
+            orm.sessionmaker(bind=self.engine, autocommit=False)
+        )
+
+    @property
+    def session(self):
+        return self.DBSession()
+
+    def setup(self):
+        try:
+            models.SqlAlchemyModel.metadata.create_all(self.engine)
+        except Exception as e:
+            print('Não foi possivel gerar o Banco de Dados: {}'.format(e))
