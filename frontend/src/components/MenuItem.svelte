d241a38b12e4c5e7158fd70eb58c28c3eec171fc
Jorge Klemm <jorgeklemm@gmail.com>
2020-12-31 13:37:39 -0300

    Separa layout e componentes

---
+++ frontend/src/components/MenuItem.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let name = "";
+  export let href = "";
+  export let active = false;
+</script>
+
+<li class="nav-item">
+  <a class="nav-link" href="{ href }" class:active>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+    { name }
+    {#if active}
+      <span class="sr-only">(current)</span>
+    {/if}
+  </a>
+</li>
+
+<style>
+  .active {
+    color: #007bff;
+  }
+
+  .feather {
+    width: 16px;
+    height: 16px;
+    vertical-align: text-bottom;
+  }
+</style>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,13 @@
 import Dashboard from './views/dashboard.svelte'
 import Pedidos from './views/pedidos.svelte'
 import Clientes from './views/clientes.svelte'
+import Layout from './views/layout.svelte'
 
 const routes = [
-  { name: '/', component: Dashboard },
-  { name: '/dashboard', component: Dashboard },
-  { name: '/pedidos', component: Pedidos },
-  { name: '/clientes', component: Clientes },
+  { name: '/', component: Dashboard, layout: Layout },
+  { name: '/dashboard', component: Dashboard, layout: Layout },
+  { name: '/pedidos', component: Pedidos, layout: Layout },
+  { name: '/clientes', component: Clientes, layout: Layout },
 ]
 
 export { routes }
